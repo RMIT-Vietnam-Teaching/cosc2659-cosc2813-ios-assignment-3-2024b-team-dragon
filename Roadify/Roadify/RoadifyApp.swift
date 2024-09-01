0409236a3ce2481be6a1c9a13e7109cc38fc964a
kys1kh6n9dau <voduycuonggli@gmail.com>
2024-09-01 23:02:17 +0700

    new repo

---
+++ Roadify/Roadify/RoadifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadifyApp.swift
+//  Roadify
+//
+//  Created by Cường Võ Duy on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RoadifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
