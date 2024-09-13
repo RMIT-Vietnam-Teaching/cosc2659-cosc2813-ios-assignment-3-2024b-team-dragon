787f55406068d8b277b069f869f734a4ea0ad81f
Seanvt8903 <seanntd893@gmail.com>
2024-09-13 16:05:36 +0700

    SignInView Update 1 & AccountView Update 1

---
+++ Roadify/Roadify/ViewModels/AccountViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AccountViewModel.swift
+//  Roadify
+//
+//  Created by Nguyễn Tuấn Dũng on 13/9/24.
+//
+
+import SwiftUI
+
+struct AccountViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountViewModel()
+}
