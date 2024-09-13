737162603df3ca0a135f4ff0a2406fcca987f1fa
Seanvt8903 <seanntd893@gmail.com>
2024-09-13 19:28:13 +0700

    EditProfileView Update 1

---
+++ Roadify/Roadify/Views/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Roadify
+//
+//  Created by Nguyễn Tuấn Dũng on 13/9/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
