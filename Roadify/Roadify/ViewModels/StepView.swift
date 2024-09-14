9cbc89d46e45e13f43193ad11ef4b36327052c29
kys1kh6n9dau <voduycuonggli@gmail.com>
2024-09-14 10:53:32 +0700

    add welcomeView

---
+++ Roadify/Roadify/ViewModels/StepView.swift
@@ -0,0 +1,67 @@
+//
+//  StepView.swift
+//  Roadify
+//
+//  Created by Cường Võ Duy on 14/9/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct StepView: View {
+	var image: String
+	var title: String
+	var description: String
+	var onNext: () -> Void  // Closure for button action
+	
+	var progress: CGFloat
+	
+	var body: some View {
+		VStack(spacing: 20) {
+			Spacer()
+			
+			Image(image)
+				.resizable()
+				.scaledToFit()
+				.frame(height: 300)
+				.cornerRadius(10)
+				.padding()
+			
+			Text(title)
+				.foregroundColor(Color.white)
+				.font(.largeTitle)
+				.bold()
+				.multilineTextAlignment(.center)
+				.padding(.horizontal)
+			
+			Text(description)
+				.foregroundColor(Color.white)
+				.font(.title3)
+				.multilineTextAlignment(.center)
+				.padding(.horizontal, 30)
+			
+			Spacer()
+			
+			ZStack {
+				Circle()
+					.stroke(Color.gray.opacity(0.3), lineWidth: 3)
+					.frame(width: 50, height: 50)
+				
+				Circle()
+					.trim(from: 0, to: progress)
+					.stroke(Color.white, lineWidth: 3)
+					.rotationEffect(.degrees(-90))
+					.frame(width: 50, height: 50)
+				
+				Button(action: onNext) {
+					Image(systemName: "arrow.right.circle.fill")
+						.font(.system(size: 30))
+						.foregroundColor(.white)
+				}
+			}
+			.padding()
+		}
+		.padding()
+		.background(Color("Primary"))
+	}
+}
