3f3363a9e7ebfe98515a0bd815088717f248c4f5
Seanvt8903 <seanntd893@gmail.com>
2024-09-17 18:58:56 +0700

    AccountNotLogin basic

---
+++ Roadify/Roadify/Utils/UI/GIFImage.swift
@@ -0,0 +1,18 @@
+//
+//  GIFImage.swift
+//  Roadify
+//
+//  Created by Nguyễn Tuấn Dũng on 17/9/24.
+//
+
+import SwiftUI
+
+struct GIFImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GIFImage()
+}
