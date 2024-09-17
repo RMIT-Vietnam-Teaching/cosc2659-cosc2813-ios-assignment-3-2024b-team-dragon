76cca905122dff27722d29b99bc5d1f84e4f3afb
Seanvt8903 <seanntd893@gmail.com>
2024-09-18 02:11:58 +0700

    Auto Login after turn off app #1

---
+++ Roadify/Roadify/Services/AuthManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthManager.swift
+//  Roadify
+//
+//  Created by Nguyễn Tuấn Dũng on 18/9/24.
+//
+
+import SwiftUI
+
+struct AuthManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthManager()
+}
