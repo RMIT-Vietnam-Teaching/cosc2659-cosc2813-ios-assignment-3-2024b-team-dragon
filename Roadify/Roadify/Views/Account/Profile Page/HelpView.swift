082516a0f7812217782e761e488a81c25d5c95f1
Seanvt8903 <seanntd893@gmail.com>
2024-09-18 19:51:59 +0700

    HelpView and ReportABugVew Update

---
+++ Roadify/Roadify/Views/Account/Profile Page/HelpView.swift
@@ -0,0 +1,18 @@
+//
+//  HelpView.swift
+//  Roadify
+//
+//  Created by Nguyễn Tuấn Dũng on 18/9/24.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpView()
+}
---
+++ Roadify/Roadify/Views/Account/Profile Page/ReportABugView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportABugView.swift
+//  Roadify
+//
+//  Created by Nguyễn Tuấn Dũng on 18/9/24.
+//
+
+import SwiftUI
+
+struct ReportABugView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportABugView()
+}
