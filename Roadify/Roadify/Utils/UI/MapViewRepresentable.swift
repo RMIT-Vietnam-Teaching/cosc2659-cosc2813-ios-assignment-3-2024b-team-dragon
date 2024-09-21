93c932ac12d88b91952a53d9403ab4556001ec08
kys1kh6n9dau <voduycuonggli@gmail.com>
2024-09-21 18:30:49 +0700

    remove keep updating location

--- Roadify/Roadify/Utils/UI/MapViewRepresentable.swift
+++ Roadify/Roadify/Utils/UI/MapViewRepresentable.swift
@@ -64,11 +64,11 @@ struct MapViewRepresentable: UIViewRepresentable {
             }
         }
         
-        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-            let centerCoordinate = mapView.centerCoordinate
-            parent.selectedCoordinate = centerCoordinate
-//            print("MapView: Region changed. Center coordinates - Latitude: \(centerCoordinate.latitude), Longitude: \(centerCoordinate.longitude)")
-        }
+//        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//            let centerCoordinate = mapView.centerCoordinate
+//            parent.selectedCoordinate = centerCoordinate
+////            print("MapView: Region changed. Center coordinates - Latitude: \(centerCoordinate.latitude), Longitude: \(centerCoordinate.longitude)")
+//        }
         
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             let identifier = "AccidentPin"
